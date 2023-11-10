6080f402f5fee1f5b67e59a3fa5ab0f2768d031f
sehercaglayan <csehere@gmail.com>
2023-11-10 15:02:13 +0300

    webuygulamalarısonhali

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const db_connect=require('./db/mysql_connect')    //mysql connecti require ettik
 const router=require('./routers')    //router klasörünü aldığımız an otomatik olarak indexlemiş oluyoruz.
-require('dotenv/config')
+
 const app = express()
 require('dotenv/config')
 app.get('/', function (req, res) {
@@ -12,5 +12,5 @@ app.get('/app', function (req, res) {
   })
   
 app.use(express.json({limit:'50mb',extended:true,paramaterLimit:50000}))
-app.use('/api,router')
+app.use('/api',router)
 app.listen(process.env.PORT)                       //app.listen(process.env.PORT)
--- routers/routes.js
+++ routers/routes.js
@@ -1,7 +1,7 @@
 const router=require('express').Router()
 const {login,register}=require('../controllers/controller')
-
+router.post("/Login",login)
 router.post("register",register)
-
-//post:veri gönderme, get:veri alma, put :veri güncelleme,patch:veri güncelleme
+//post:Veri gönderme  get:veri alma    put:veri güncelleme   patch:veri güncelleme 
+//delete:veri silme 
 module.exports=router
